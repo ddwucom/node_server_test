3c116226be7ca49fee1d5f2e288dc44e344ab29b
ddwucom <ddwucom@gmail.com>
2020-01-15 21:57:12 +0900

    server get and post test

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var http = require('http');
-var url = require('url');
-var querystring = require('querystring');
+var fs = require('fs');
  
 http.createServer(function (request, response) {
-    var query = url.parse(request.url, true).query;
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end('<h1>' + JSON.stringify(query) + '</h1>');
+    if (request.method == 'GET') {
+        fs.readFile('./html/login.html', function (error, data) {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.end(data);
+        });
+    } else if (request.method == 'POST') {
+        request.on('data', function(data) {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.end('<h1>' + data + '</h1>');
+        });
+    }
 }).listen(1234, function() {
     console.log('Server running at http://127.0.0.1:1234');
 });
-
