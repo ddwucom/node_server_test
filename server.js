d54154fe92f02f24e31998c19f05453a149d279c
ddwucom <ddwucom@gmail.com>
2020-01-15 21:33:57 +0900

    server variety page test

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var http = require('http');
-
-http.createServer(function (request, response) {
-    response.writeHead(302, { 'Location': 'https://cs.dongduk.ac.kr'});
-    response.end();
+var fs = require('fs');
+var url = require('url');
+ 
+http.createServer( function (request, response) {
+    var pathName = url.parse(request.url).pathname;
+    if (pathName == '/') {
+        fs.readFile('./html/index.html', function(error, data) {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.end(data);
+        });
+    } else if (pathName == '/second') {
+        fs.readFile('./html/second_page.html', function(error, data) {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.end(data);
+        });
+    }
 }).listen(1234, function() {
     console.log('Server running at http://127.0.0.1:1234');
 });
